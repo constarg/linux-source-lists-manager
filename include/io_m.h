c9e1dd05cc73a40b590df7a12b0d7bdba8764180
Constantinos Argyriou <rounnus@gmail.com>
2022-06-15 14:20:44 +0300

    define the structure of the project.

---
+++ include/io_m.h
@@ -0,0 +1,16 @@
+#ifndef IO_MANAGER_H
+#define IO_MANAGER_H
+
+
+extern int get_source_files(char *dst);
+
+/**
+ * Read the content of a source file.
+ */
+extern int read_source_file(char *dst, const char *name);
+
+extern int write_source_file(const char *src, const char *path):
+
+
+
+#endif
---
+++ src/io_m.c
@@ -0,0 +1,22 @@
+
+
+#include "io_m.h"
+
+#define SOURCE_LIST     "/etc/apt/source.list"
+#define SOURCE_LIST_D   "/etc/apt/sources.list.d/"
+
+
+int get_source_files(char *dst)
+{
+
+}
+
+int read_source_file(char *dst, const char *name)
+{
+
+}
+
+int write_source_file(const char *src, const char *path)
+{
+
+}
