7f2ae62a3fe6015c924b3fc87fedfbeda1caee4e
Constantinos Argyriou <constarg@pm.me>
2022-07-04 00:21:34 +0300

    almost done with open source list function

--- include/io_m.h
+++ include/io_m.h
@@ -2,6 +2,7 @@
 #define IO_MANAGER_H
 
 #include <malloc.h>
+#include <stdlib.h>
 
 static inline void close_source_files_d(char **src)
 {
@@ -22,6 +23,7 @@ extern int read_source_file(char **dst, const char *path);
 
 extern int write_source_file(const char *src, const char *path);
 
+extern char **retv_file_lines(const char *path, size_t *lines_s);
 
 
 #endif
